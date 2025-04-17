4cd92491ff290ffc13e527ca799b418d3da23d8f
karoluciecha <g00436758@atu.ie>
2025-04-17 18:10:01 +0200

    Reinstalled PWA

--- planting-habits/src/main.ts
+++ planting-habits/src/main.ts
@@ -46,6 +46,9 @@ bootstrapApplication(AppComponent, {
           }), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
           }),
   ],
 });
